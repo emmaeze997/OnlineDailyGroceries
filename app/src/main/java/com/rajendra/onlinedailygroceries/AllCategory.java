4c3e81fadd665044b24ca2ff943c31de18c26c68
emmaeze997 <120291331+emmaeze997@users.noreply.github.com>
2022-12-13 05:17:16 -1200

    good app.....would like to use it as my project

--- app/src/main/java/com/rajendra/onlinedailygroceries/AllCategory.java
+++ app/src/main/java/com/rajendra/onlinedailygroceries/AllCategory.java
@@ -77,9 +77,9 @@ public class AllCategory extends AppCompatActivity {
 
     public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
 
-        private int spanCount;
-        private int spacing;
-        private boolean includeEdge;
+        private final int spanCount;
+        private final int spacing;
+        private final boolean includeEdge;
 
         public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
             this.spanCount = spanCount;
